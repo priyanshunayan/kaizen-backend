f0086e455845fb836c0ee76f8e7f5f2dfe2d9054
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-01-24 16:01:06 +0530

    favorite and read route create

--- api/routes/books.js
+++ api/routes/books.js
@@ -22,13 +22,88 @@ router.post('/toread', (req, res, next) => {
                 throw err;
             } else {
                 user.toread.push(book._id);
-                res.status(200).json({
-                    message: "Book Added Suuccessfully",
-                    data: user
+                user.save((err, user) => {
+                    if(err) {
+                        res.status(500).json({
+                            message: "Error while adding"
+                        })
+                    } else {
+                        res.status(200).json({
+                            message: "Book Added Suuccessfully",
+                            data: user
+                        })
+                    }
+                    
+                })
+                
+            }
+        })
+    })
+})
+router.post('/read', (req, res, next) => {
+    User.findById(req.body.userId).exec().then((user) => {
+        const book = new Books({
+            _id: mongoose.Types.ObjectId(),
+            name: req.body.name,
+            authors: req.body.authors,
+            subtitle: req.body.subtitle,
+            thumbnail: req.body.thumbnail,
+            description: req.body.description,
+            userId: req.body.userId
+        });
+        book.save((err, book) => {
+            if(err) {
+                throw err;
+            } else {
+                user.read.push(book._id);
+                user.save((err, user) => {
+                    if(err) {
+                        res.status(500).json({
+                            message: "Error while adding"
+                        })
+                    } else {
+                        res.status(200).json({
+                            message: "Book Added Suuccessfully",
+                            data: user
+                        })
+                    }
+                    
                 })
             }
         })
     })
 })
-
+router.post('/favourite', (req, res, next) => {
+    User.findById(req.body.userId).exec().then((user) => {
+        const book = new Books({
+            _id: mongoose.Types.ObjectId(),
+            name: req.body.name,
+            authors: req.body.authors,
+            subtitle: req.body.subtitle,
+            thumbnail: req.body.thumbnail,
+            description: req.body.description,
+            userId: req.body.userId
+        });
+        book.save((err, book) => {
+            if(err) {
+                throw err;
+            } else {
+                user.favourite.push(book._id);
+                user.save((err, user) => {
+                    if(err) {
+                        res.status(500).json({
+                            message: "Error while adding"
+                        })
+                    } else {
+                        res.status(200).json({
+                            message: "Book Added Suuccessfully",
+                            data: user
+                        })
+                    }
+                    
+                })
+            }
+        })
+    })
+})
 module.exports = router;
